5043d8e43cf7b51f82766b44963d54c8d8f5fc10
Subodh Dahal <contact.subodhdahal@gmail.com>
2018-08-16 22:04:15 +0200

    Enabled CORS for all requests

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import http from 'http'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 import scrapeCtrl from './scrapeCtrl'
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
+app.use(cors())
+
 const server = http.Server(app)
 
 /** Route plan -- START */
